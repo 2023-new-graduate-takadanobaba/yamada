878facb93cffb580ec954edec292a268a2ff2be5
sknkn <syouya.yamada@goodsun-hd.com>
2023-06-22 15:35:55 +0900

    Update Sum.java

--- todo/src/sum/Sum.java
+++ todo/src/sum/Sum.java
@@ -1,6 +1,35 @@
+package sum;
+
+import java.util.Scanner;
+
 public class Sum {
-    public static void main(String[] args) throws Exception {
-         System.out.println("Hello");
+    public static void main(String[] args){
+        
+        double sum = 0;
+        double num = 0;
+        String str = "";
+
+        System.out.println("合計を求めます");
+
+        do {
+
+            System.out.println("数値を入力してください。合計を表示するには「end」と入力してください。");
+
+            Scanner scanner = new Scanner(System.in);
+            str = scanner.nextLine();
+            //System.out.println(str);
+
+            try {
+                num = Double.parseDouble(str);
+            } catch(NumberFormatException e){
+                continue;
+            }
+
+            sum += num;
+
+        }while(!str.equals("end"));
+
+        System.out.println("合計："+sum);
     }
 
 }
